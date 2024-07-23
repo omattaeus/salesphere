42984bb079a760943e407ed2bfbfb5e3dd828e0e
Mateus Gomes <127147447+omattaeus@users.noreply.github.com>
2024-07-23 19:28:05 -0300

    Inital Commit

---
+++ src/main/java/com/salesphere/salesphere/SalesphereApplication.java
@@ -0,0 +1,13 @@
+package com.salesphere.salesphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesphereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salesphere/salesphere/SalesphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesphere.salesphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
